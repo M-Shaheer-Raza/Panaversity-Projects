a9b9ba36ce0d8c19c17035f53936ceebf497957b
M-Shaheer-Raza <shaheer.raza302@gmail.com>
2025-02-19 13:56:32 +0500

    Update Hangman Python Project

--- 5. Hangman Python Project/hangman.py
+++ 5. Hangman Python Project/hangman.py
@@ -1,59 +1,47 @@
 import random
+import string
+from words import words
+from hangman_visual import lives_visual_dict
 
-def choose_word(word_list):
-    return random.choice(word_list)
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()
 
-def display_current_state(secret_word, guessed_letters):
-    return " ".join([letter if letter in guessed_letters else "_" for letter in secret_word])
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
 
-def get_user_guess(used_letters):
-    while True:
-        guess = input("Guess a letter: ").strip().lower()
-        if len(guess) == 1 and guess.isalpha() and guess not in used_letters:
-            return guess
-        print("Invalid input or already guessed. Try again.")
+    lives = 7
 
-def play_hangman():
-    word_list = ['python', 'java', 'hangman', 'challenge', 'programming', 'developer']
-    secret_word = choose_word(word_list)
-    guessed_letters = set()
-    wrong_guesses = set()
-    allowed_mistakes = 6
+    while len(word_letters) > 0 and lives > 0:
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
-    print("\nWelcome to Hangman!")
-    print("Try to guess the secret word, one letter at a time.")
-    print(f"You are allowed {allowed_mistakes} incorrect guesses.")
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(lives_visual_dict[lives])
+        print('Current word: ', ' '.join(word_list))
 
-    while True:
-        print("\nWord: " + display_current_state(secret_word, guessed_letters))
-        print("Wrong guesses:", " ".join(sorted(wrong_guesses)))
-        print("Remaining mistakes:", allowed_mistakes - len(wrong_guesses))
-
-        guess = get_user_guess(guessed_letters.union(wrong_guesses))
-
-        if guess in secret_word:
-            guessed_letters.add(guess)
-            print("Good guess!")
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives -= 1
+                print('\nYour letter,', user_letter, 'is not in the word.')
+        elif user_letter in used_letters:
+            print('\nYou have already used that letter. Guess another letter.')
         else:
-            wrong_guesses.add(guess)
-            print("Sorry, that letter is not in the word.")
+            print('\nThat is not a valid letter.')
 
-        if all(letter in guessed_letters for letter in secret_word):
-            print("\nCongratulations! You guessed the word:", secret_word)
-            break
+    if lives == 0:
+        print(lives_visual_dict[lives])
+        print('You died, sorry. The word was', word)
+    else:
+        print('YAY! You guessed the word', word, '!!')
 
-        if len(wrong_guesses) >= allowed_mistakes:
-            print("\nGame Over! You've run out of guesses.")
-            print("The secret word was:", secret_word)
-            break
-
-def main():
-    while True:
-        play_hangman()
-        replay = input("\nDo you want to play again? (yes/no): ").strip().lower()
-        if replay not in ['yes', 'y']:
-            print("Thanks for playing Hangman! Goodbye.")
-            break
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    hangman()
